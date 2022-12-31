fc0a1bea9be3a428255e1806cef329daeb72f552
Mark Saunders <301488+invalidopc0de@users.noreply.github.com>
2022-12-31 12:58:48 -0800

    Enable touch controller for STM32F469 Disco board

--- samples/drivers/kscan_touch/src/main.c
+++ samples/drivers/kscan_touch/src/main.c
@@ -10,15 +10,17 @@
 #include <soc.h>
 #include <zephyr/drivers/kscan.h>
 
+#define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
+
 #define LOG_LEVEL LOG_LEVEL_DBG
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main);
 
 const struct device *const kscan_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_keyboard_scan));
+const struct gpio_dt_spec signal = GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, signal_gpios);
 
-static void k_callback(const struct device *dev, uint32_t row, uint32_t col,
-		       bool pressed)
+static void k_callback(const struct device *dev, uint32_t row, uint32_t col, bool pressed)
 {
 	ARG_UNUSED(dev);
 	if (pressed) {
@@ -30,9 +32,16 @@ void main(void)
 {
 	printk("Kscan touch panel sample application\n");
 
+	/* Configure the pin */
+	gpio_pin_configure_dt(&signal, GPIO_OUTPUT_INACTIVE);
+	/* Set the pin to its active level */
+	gpio_pin_set_dt(&signal, 1);
+
 	if (!device_is_ready(kscan_dev)) {
 		LOG_ERR("kscan device %s not ready", kscan_dev->name);
 		return;
+	} else {
+		LOG_INF("kscan device %s ready", kscan_dev->name);
 	}
 
 	kscan_config(kscan_dev, k_callback);
